f04f296652b4ac6facd76585ad054b61b45f15b6
BonghoCha <37327313+BonghoCha@users.noreply.github.com>
2022-04-14 23:59:20 +0900

    update: ~24강까지 내용 정리

--- CSharpPractice/CSharpPractice/Program.cs
+++ CSharpPractice/CSharpPractice/Program.cs
@@ -549,10 +549,104 @@ namespace CSharpPractice
 
         class TEXTRPG
         {
+            enum ClassType
+            {
+                None = 0,
+                Knight = 1,
+                Archer = 2,
+                Mage = 3
+            }
+
+            ClassType ChooseClass()
+            {
+                Console.WriteLine("직업을 선택하세요.");
+                Console.WriteLine("[1] 기사");
+                Console.WriteLine("[2] 궁수");
+                Console.WriteLine("[3] 법사");
+
+                ClassType choice = ClassType.None;
+
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        {
+                            choice = ClassType.Knight;
+                            break;
+                        }
+                    case "2":
+                        {
+                            choice = ClassType.Archer;
+                            break;
+                        }
+                    case "3":
+                        {
+                            choice = ClassType.Mage;
+                            break;
+                        }
+                }
+
+                return choice;
+            }
+
+            void CreatePlayer(ClassType choice, out Player player)
+            {
+                player.type = choice;
+
+                switch (player.type)
+                {
+                    case ClassType.Knight:
+                        {
+                            player.hp = 100;
+                            player.attack = 10;
+                            break;
+                        }
+                    case ClassType.Archer:
+                        {
+                            player.hp = 75;
+                            player.attack = 12;
+                            break;
+                        }
+                    case ClassType.Mage:
+                        {
+                            player.hp = 50;
+                            player.attack = 15;
+                            break;
+                        }
+                    default: {
+                            player.hp = 0;
+                            player.attack = 0;
+                            break;
+                        }
+                }
+            }
+
+            struct Player
+            {
+                public ClassType type;
+
+                public int hp;
+                public int attack;
+            }
+
             public TEXTRPG()
             {
                 // 디버그
-                // 브레이킹 포인트 설정 > F10(다음 라인), F11(메소드 안까지 디버그)
+                // 브레이킹 포인트 설정 > F10(다음 라인), F11(메소드 안까지 디버그)                
+
+                while (true)
+                {
+                    ClassType choice = ChooseClass();
+
+                    if (!choice.Equals(ClassType.None))
+                    {
+                        Player player;
+
+                        // 캐릭터를 선택했을 때 캐릭터 생성
+                        CreatePlayer(choice, out player);
+                    }
+                }
             }
 
             public int Add(int a, int b)
@@ -564,8 +658,6 @@ namespace CSharpPractice
         static void Main(string[] args)
         {
             TEXTRPG t = new TEXTRPG();
-            int ret = t.Add(10, 20);
-            Console.WriteLine(ret);
         }
     }
 }
